c4c2241bbc094c6cc0824403f8eb4adc78372641
Antonio V. Leonti <53806445+antoniovleonti@users.noreply.github.com>
2020-11-22 18:17:26 -0500

    Added dedicated input / output layer

--- ascii_chess_ii/Program.cs
+++ ascii_chess_ii/Program.cs
@@ -8,27 +8,59 @@ namespace ASCII_Chess_II
     {
         static void Main(string[] args)
         {
-            Stopwatch stopWatch = new Stopwatch();
-            // set up piece state
-            int[,] piece =
+            Game game = new Game();
+
+            int? result = null;
+            int lastPlayer;
+
+            while (result == null)
             {
-                { 4, 2, 3, 6, 5, 3, 2, 4},
-                { 1, 1, 1, 1, 1, 1, 1, 1},
-                { 0, 0, 0, 0, 0, 0, 0, 0},
-                { 0, 0, 0, 0, 0, 0, 0, 0},
-                { 0, 0, 0, 0, 0, 0, 0, 0},
-                { 0, 0, 0, 0, 0, 0, 0, 0},
-                {-1,-1,-1,-1,-1,-1,-1,-1},
-                {-4,-2,-3,-6,-5,-3,-2,-4}
-            };
+                // print board state
+                Console.WriteLine(game.Board.ToString(game.Player));
 
-            // set up touch state
-            uint[,] touch = new uint[8, 8];
+                string s = Console.ReadLine(); // get player input
+                Move input = ReadMove(s);
+                
+                lastPlayer = game.Player; // get current player
 
-            // new board using pieces & touch state
-            Board board = new Board(piece, touch);
+                if (input == null)
+                {
+                    Console.WriteLine("\t(?) Cannot parse move!");
+                }
 
-            Manager.PlayChessGame(board);
+                // try to make this move
+                result = game.TryMove(input);
+
+                // if it's not the other player's turn now, move was illegal.
+                if (game.Player == lastPlayer)
+                {
+                    Console.WriteLine("\t(!) Illegal move!");
+                }
+            }
+        }
+
+        private static Move ReadMove(string s)
+        {
+            if (s.Length < 4) return null;
+
+            string abcs = "hgfedcba";
+            string nums = "12345678";
+
+            int[] t = { -1, -1, -1, -1, -1 };
+
+            // get coords
+            for (int i = 0; i < 4; i++)
+            {
+                t[i] = abcs.IndexOf(s[i]);
+                if (t[i] == -1) t[i] = nums.IndexOf(s[i]);
+
+                if (t[i] == -1) return null;
+            }
+
+            Move move = 
+                new Move(new Pos2(t[1], t[0]), new Pos2(t[3], t[2]));
+
+            return move;
         }
     }
 }
