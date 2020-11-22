1b35cc501e9fc44a340ae1c2a6e973d8fe03d65e
Antonio V. Leonti <53806445+antoniovleonti@users.noreply.github.com>
2020-11-22 18:17:05 -0500

    Created interface IPiece
    An interface is a contract .... a promise. Classes deriving the IPiece interface promise to implement ListMoves() for use in the class Board.
    In the future, I plan to add a MakeMove() method to this contract, taking the special case responsiblity away from Board.MakeMove().

---
+++ ascii_chess_ii/Pieces/IPiece.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASCII_Chess_II
+{
+    interface IPiece
+    {
+        public abstract (List<Pos2> m, List<Pos2> c) ListMoves(Board board, Pos2 pos, int player);
+
+        protected static (List<Pos2> m, List<Pos2> c)
+            CastMoves(Board board, Pos2 pos, int dx, int dy, int player)
+        {
+            List<Pos2> maneuvers = new List<Pos2>();
+            List<Pos2> captures = new List<Pos2>();
+
+            int y, x;
+            for (int i = 1; true; i++)
+            {
+                y = pos.y + dy * i;
+                x = pos.x + dx * i;
+
+                if (y >= 0 && y < 8 // within bounds
+                && x >= 0 && x < 8 // ^^^
+                && Math.Sign(board.Value[y, x]) != player) // is not friendly piece
+                {
+                    if (Math.Sign(board.Value[y, x]) == -player)
+                    {
+                        captures.Add(new Pos2(y, x));
+                        return (maneuvers, captures);
+                    }
+                    else maneuvers.Add(new Pos2(y, x));
+                }
+                else return (maneuvers, captures);
+            }
+        }
+    }
+}
